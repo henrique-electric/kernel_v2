16c24c6b3a8901074af93555f061c4e012d84ae3
José Henrique <146463048+henrique-electric@users.noreply.github.com>
2025-09-11 20:51:25 -0300

    Create an gdt

---
+++ include/gdt.h
@@ -0,0 +1,23 @@
+#pragma once
+
+
+#include <types.h>
+#include <stdint.h>
+
+#pragma packed
+struct gdt_entry {
+    uint16_t limit;
+    uint16_t base;
+    uint8_t  base_mid;
+    uint8_t  access;
+    uint8_t  limit_and_flag;
+    uint8_t  base_high;
+};
+
+#pragma packed
+struct gdt_table {
+    uint16_t    size;
+    uint32_t    offset;
+};
+
+void load_gdt(void);
---
+++ src/kernel/gdt.c
@@ -0,0 +1,17 @@
+#include <gdt.h>
+
+static struct gdt_entry entries[3] = {
+    {0},
+    {.limit = 0xFFFF, .base = 0x0000, .base_mid = 0x00, .access = 0x9A, .limit_and_flag = 0xCF, .base_high = 0x00},
+    {.limit = 0xFFFF, .base = 0x0000, .base_mid = 0x00, .access = 0x92, .limit_and_flag = 0xCF, .base_high = 0x00}
+};
+
+static struct gdt_table table = {
+    .size = (uint32_t) sizeof(entries) - 1,
+    .offset = (uint32_t) entries,
+};
+
+void load_gdt(void) {
+    asm volatile("lgdt (%0)" : : "r" (&table));
+}
+
