3b00a328bed4371e8364df7a5714ffa015a29738
José Henrique <146463048+henrique-electric@users.noreply.github.com>
2025-09-12 20:10:53 -0300

    add a support for the new line character

--- src/kernel/tty/puts.c
+++ src/kernel/tty/puts.c
@@ -1,13 +1,19 @@
 #include <libc/puts.h>
 
+uint32_t vga_count = 0;
+
 void puts(char *str) {
     char *vga_buff = (char*) 0xB8000;
 
     uint16_t str_count = 0;
-    uint16_t vga_count = 0;
     while (str[str_count] != 0x00) {
+        if (str[str_count] == '\n') {
+            str[str_count] = ' ';
+            vga_count  = ((((vga_count - 1) + (80-1)*2) / (80*2)) * (80*2)) - 2;
+        }
+
         vga_buff[vga_count] = str[str_count];
-        vga_buff[vga_count + 1] = 0x01;
+        vga_buff[vga_count + 1] = 0x07;
         str_count++;
         vga_count +=2;
     }
