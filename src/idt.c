3734318f0172a9b4b82036f5ca5ce682ae5afe26
José Henrique <146463048+henrique-electric@users.noreply.github.com>
2025-09-11 21:11:36 -0300

    How could i make such mistake like that

--- src/idt.c
+++ src/idt.c
@@ -7,14 +7,21 @@ static struct idtr idt_table = {
     .offset = (uint32_t) handlers
 };
 
-void set_entry(uint32_t handler, uint16_t index) {
-    handlers[handler].offset_low = handler & 0xff;
-    handlers[handler].segment_selector = 0x08;
-    handlers[handler].reserved = 0x00;
-    handlers[handler].ist = 0b11100111;
-    handlers[handler].offset_high = (handler >> 24) & 0xFF;
+void key_handler() {
+    puts("Key\0");
+}
+
+void set_entry(uint32_t handler, uint8_t index) {
+    handlers[index].offset_low = handler & 0xFFFF;             // lower 16 bits
+    handlers[index].segment_selector = 0x08;                   // kernel code segment
+    handlers[index].reserved = 0;                               // reserved, set 0
+    handlers[index].flags = 0x8E;                               // present, interrupt gate, ring 0
+    handlers[index].offset_high = (handler >> 16) & 0xFFFF;    // upper 16 bits
 }
 
 void load_idt(void) {
+    set_entry((uint32_t) key_handler, 0x21);
+    asm("cli");
     asm volatile("lidt (%0)" : : "r" (&idt_table));
+    asm("sti");
 }
