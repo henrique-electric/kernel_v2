8c2bd85e1c70c57e640130b12800f8130c8d822a
José Henrique <146463048+henrique-electric@users.noreply.github.com>
2025-09-12 11:30:50 -0300

    backup

---
+++ src/kernel/idt.c
@@ -0,0 +1,29 @@
+#include <idt.h>
+
+static struct gate_descriptor handlers[256];
+
+static struct idtr idt_table = {
+    .size = (uint16_t) (sizeof(struct gate_descriptor) * 256) - 1,
+    .offset = (uint32_t) handlers
+};
+
+
+__attribute__((noreturn)) void test() {
+   asm volatile("cli; hlt");
+}
+
+void set_entry(uint32_t handler, uint8_t index) {
+    handlers[index].offset_low = handler & 0xFFFF;             // lower 16 bits
+    handlers[index].segment_selector = 0x08;                   // kernel code segment
+    handlers[index].reserved = 0;                               // reserved, set 0
+    handlers[index].flags = 0x8E;                               // present, interrupt gate, ring 0
+    handlers[index].offset_high = (handler >> 16) & 0xFFFF;    // upper 16 bits
+}
+
+void load_idt(void) {
+    for (uint8_t i = 0; i < 32; i++)
+        set_entry((uint32_t) test, i);
+    
+    asm volatile("lidt %0" : : "r" (&idt_table));
+    asm volatile("sti");
+}
