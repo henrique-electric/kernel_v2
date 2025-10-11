3f22eeae79f8c8fab20bbd1a6c2990591012adb3
José Henrique <146463048+henrique-electric@users.noreply.github.com>
2025-10-10 21:36:01 -0300

    Create a file to declare function defined in assembly

---
+++ include/asm_wrappers.h
@@ -0,0 +1,19 @@
+#ifndef ASM_WRAPPERS_H
+#define ASL_WRAPPERS_H
+#include <stdint.h>
+
+// ====== Port/IO======
+extern void outb(uint16_t port, uint16_t value);
+extern uint16_t inb(uint16_t port);
+// ====================
+
+// ====== Tables ======
+extern void load_gdt_table(void *table);
+extern void load_idt_table(void *table);
+// ====================
+
+// ====== ISR handlers ======
+//extern void isr
+
+// =========================
+#endif
