f863357c65e82b03840c52c41f773b141fcc3b7d
José Henrique <146463048+henrique-electric@users.noreply.github.com>
2025-10-10 21:38:57 -0300

    use in and out defined in asm

---
+++ src/arch/ports.asm
@@ -0,0 +1,31 @@
+global outb
+global inb
+
+section .text
+inb:
+    push ebp
+    push dx
+
+    mov dx, [ebp + 8]
+    
+    in ax, dx
+
+
+    pop dx
+    pop ebp
+    ret
+
+outb:
+    push ebp
+    push dx
+    push ax
+
+    mov dx, [ebp + 12]
+    mov ax, [ebp + 8]
+
+    out dx, ax
+
+    pop ax
+    pop dx
+    pop ebp
+    ret
