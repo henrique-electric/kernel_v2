daea9a14f4e71bd1a6deb527875a53ea5faaf25a
José Henrique <146463048+henrique-electric@users.noreply.github.com>
2025-10-12 02:54:11 -0300

    Add assembly helpers

--- include/asm_wrappers.h
+++ include/asm_wrappers.h
@@ -2,6 +2,12 @@
 #define ASL_WRAPPERS_H
 #include <stdint.h>
 
+// ====== Assembly helpers ========
+#define CLI() asm __volatile__("cli");
+#define STI() asm __volatile__("sti");
+#define HLT() asm __volatile__("hlt");
+// ================================
+
 // ====== Port/IO======
 extern void outb(uint16_t port, uint16_t value);
 extern uint16_t inb(uint16_t port);
