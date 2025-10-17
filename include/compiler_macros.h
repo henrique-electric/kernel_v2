8f215de69755dc89cc3ca06100160d6222745dee
José Henrique <146463048+henrique-electric@users.noreply.github.com>
2025-10-16 23:19:40 -0300

    File with compiler macros
    Create a separated file to store macros to pass compiler attributes
    inside the code

---
+++ include/compiler_macros.h
@@ -0,0 +1,8 @@
+#ifndef COMPILER_MACROS_H
+#define COMPILER_MACROS_H
+
+#define PACKED __attribute__((packed))
+#define INTERRUPT __attribute__((interrupt))
+#define CDECL __attribute__((cdecl))
+
+#endif // !COMPILER_MACROS_H
