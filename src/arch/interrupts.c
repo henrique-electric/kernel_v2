be557c8d8af84f45b42089f815e69e5f0f490db6
José Henrique <146463048+henrique-electric@users.noreply.github.com>
2025-10-17 22:35:41 -0300

    Backup

--- src/arch/interrupts.c
+++ src/arch/interrupts.c
@@ -1,27 +1,27 @@
 #include <arch/interrupts.h>
-#include <stdint.h>
-#include <kernel/tty/kb.h>
 #include <asm_wrappers.h>
+#include <compiler_macros.h>
 #include <kernel/log.h>
+#include <kernel/tty/kb.h>
+#include <stdint.h>
 
 void print_dec(uint8_t n) {
-    volatile char* vga = (char*)0xB8000;
-    int pos = 0;
+  volatile char *vga = (char *)0xB8000;
+  int pos = 0;
 
-    if (n >= 100) {
-        vga[pos++] = '0' + (n / 100);
-        vga[pos++] = 0x07;
-    }
-    if (n >= 10) {
-        vga[pos++] = '0' + ((n / 10) % 10);
-        vga[pos++] = 0x07;
-    }
-    vga[pos++] = '0' + (n % 10);
+  if (n >= 100) {
+    vga[pos++] = '0' + (n / 100);
     vga[pos++] = 0x07;
+  }
+  if (n >= 10) {
+    vga[pos++] = '0' + ((n / 10) % 10);
+    vga[pos++] = 0x07;
+  }
+  vga[pos++] = '0' + (n % 10);
+  vga[pos++] = 0x07;
 }
 
-
-void __attribute__((cdecl))handler_isr(int id) {
+void CDECL handler_isr(int id) {
   if (id == 13) {
     log("GPF", ERROR);
   }
