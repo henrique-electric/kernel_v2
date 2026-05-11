cb36411da3cd13f99e068fcfdf3333219bb5f719
José Henrique <146463048+henrique-electric@users.noreply.github.com>
2026-05-11 07:45:30 -0300

    Fix a flaw trying to load the cs register to the correct entry in the GDT.
    Also added some comments and fixes on the tss stuff.

--- src/arch/tables.asm
+++ src/arch/tables.asm
@@ -7,7 +7,7 @@ section .text
 load_idt_table:
     push ebp
     mov ebp, esp
-    mov eax, [ebp + 8]
+    mov eax, [ebp + 8] ; This function receives a 32bit pointer as parameter that points where the IDT structure lives in memory
     lidt [eax]
     pop ebp
     sti
@@ -17,30 +17,42 @@ load_gdt_table:
     cli
     push ebp
     mov ebp, esp
-    mov eax, [ebp + 8]
+    mov eax, [ebp + 8] ; This function receives a 32bit pointer as parameter that points where the GDT structure lives in memory
     lgdt [eax]
-    pop ebp
-    mov ax, 0x08
-    mov cs, ax
+    
+    ; Perform a far jump to reload the CS register and load teh correct data segments to point to the correct entry on the GDT
+    jmp 0x08:.load_data_segs
+
+.load_data_segs:
+    ; Now reload the other data segment registers
     mov ax, 0x10
     mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
     mov ss, ax
+    
+    pop ebp
     ret
 
 load_tss_table:
     push ebp
     mov ebp, esp
-    mov ax, 0x18
+    mov ax, 0x18 ; The TSS table lives on the entry 0x18 in the GDT
     ltr ax
     pop ebp
     ret
 
+
+; This function saves teh GDT table into an adress provided
 save_gdt:
   push ebp
-  push ebx
   mov ebp, esp
+  push ebx
+
   mov ebx, [ebp + 8]
-  sgdt [ebx]
-  pop ebp
+  sgdt [ebx] ; Save the GDT table at the address passed as parameter.
+
   pop ebx
+  pop ebp
   ret
